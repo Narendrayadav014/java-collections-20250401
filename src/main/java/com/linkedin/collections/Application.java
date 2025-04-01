baa4823c8c2d2737e6ebdd06321f7cb36c57d34d
Narendrayadav014 <85392032+Narendrayadav014@users.noreply.github.com>
2025-04-01 09:45:23 +0000

    Iterator demo

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -3,6 +3,7 @@ package com.linkedin.collections;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Iterator;
 
 public class Application {
 
@@ -13,7 +14,31 @@ public class Application {
 		Room westminister = new Room("Westminister", "Premiere Room", 4, 200.00);
 		
 		Collection<Room> rooms = new ArrayList<>(Arrays.asList(piccadilly, cambridge, westminister));
-		
+
+		Iterator<Room> iterator = rooms.iterator();
+
+		// iterator.next() method will iterate throw the next element 
+		System.out.println(iterator.next().getName());
+		System.out.println(iterator.next().getName());
+		System.out.println(iterator.next().getName());
+
+		// New instance of iterator will be created and will start from the first element
+		Iterator<Room> iterator2 = rooms.iterator();
+		System.out.println(iterator2.next().getName());
+
+		// Generally iterator is used with while loop as follows
+		Iterator<Room> iterator3 = rooms.iterator();
+
+		while(iterator3.hasNext()){
+			Room room = iterator3.next();
+			System.out.println(room.getName());
+		}
+
+		// Iterators can be replaced with the enhanced forEach loop as follows -
+
+		for(Room room1 : rooms){
+			System.out.println(room1.getName());
+		}
 			
 	}
 }
