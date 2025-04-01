d4f43c79f2e567fdc75da0a8928052a76058c8d4
Narendrayadav014 <85392032+Narendrayadav014@users.noreply.github.com>
2025-04-01 10:07:21 +0000

    Solution to ConcurrentModificationException

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -18,15 +18,45 @@ public class Application {
 		oxford.setPetFriendly(true);
 		victoria.setPetFriendly(true);
 		
-		for(Room room : rooms) {
+
+		// This will throw thw ConcurrentModificationException
+		// as we are trying to remove the element from the collection that is being traversed 
+		// for(Room room : rooms) {
 			
-			if(room.isPetFriendly()) {
-				rooms.remove(room);
+		// 	if(room.isPetFriendly()) {
+		// 		rooms.remove(room);
+		// 	}
+			
+		// }
+
+		// This can be solved by following code-
+
+		Collection<Room> removeRooms = new ArrayList<>();
+
+		for(Room room : rooms){
+			if(room.isPetFriendly()){
+				removeRooms.add(room);
 			}
-			
 		}
+		rooms.removeAll(removeRooms);
+
+		// Another way to solve this problem is by using the iterators-
+		Collection<Room> rooms2 = new ArrayList<>(Arrays.asList(cambridge, oxford, victoria, manchester));
+		oxford.setPetFriendly(true);
+		victoria.setPetFriendly(true);
+
+		Iterator<Room> iterator =  rooms2.iterator();
+
+		while(iterator.hasNext()){
+			Room r = iterator.next();
+			if(r.isPetFriendly()){
+				iterator.remove();
+			}
+		}
+
 		
 		System.out.println(rooms);
+		System.out.println(rooms2);
 		
 	}
 }
