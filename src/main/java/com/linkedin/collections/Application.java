72d9c80060121fb34811873baa8b30537379ae11
Narendrayadav014 <85392032+Narendrayadav014@users.noreply.github.com>
2025-04-04 07:43:11 +0000

    accessing elements using streams

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -3,6 +3,8 @@ package com.linkedin.collections;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class Application {
 
@@ -17,13 +19,31 @@ public class Application {
 		oxford.setPetFriendly(true);
 		victoria.setPetFriendly(true);
 		
-		for(Room room : rooms) {
+		// for(Room room : rooms) {
 			
-			if(room.isPetFriendly()) {
+		// 	if(room.isPetFriendly()) {
+		// 		System.out.println(room.getName());
+		// 	}
+			
+		// }
+
+		// filter() is an intermediate operation that uses functional interface Predicate
+		// forEach() is a terminal operation that is used to terminate the stream pipeline
+		rooms.stream()
+		.filter(new Predicate<Room>() {
+			
+			@Override
+			public boolean test(Room room){
+				System.out.format("Testing %s with result %b%n", room.getName(),room.isPetFriendly());
+				return room.isPetFriendly();
+			} 
+		}).forEach(new Consumer<Room>() {
+			
+			@Override
+			public void accept(Room room){
 				System.out.println(room.getName());
 			}
-			
-		}
+		});
 		
 	}
 }
